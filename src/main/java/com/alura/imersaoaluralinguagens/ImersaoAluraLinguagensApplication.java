ff391693f12ada36a6d4c5614ea05e415c213050
Wesley Castelani <wesley.castelani@hotmail.com>
2023-03-31 17:56:59 -0300

    Primeiro commit

---
+++ src/main/java/com/alura/imersaoaluralinguagens/ImersaoAluraLinguagensApplication.java
@@ -0,0 +1,13 @@
+package com.alura.imersaoaluralinguagens;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ImersaoAluraLinguagensApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ImersaoAluraLinguagensApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/alura/imersaoaluralinguagens/ImersaoAluraLinguagensApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alura.imersaoaluralinguagens;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ImersaoAluraLinguagensApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
